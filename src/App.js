9e291e41a269443bb11c3e0ae0a513d3c0d8e528
Milind Sangale <smilind900@gmail.com>
2024-12-13 22:28:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-/* import Textform from './components/Textform'; */
-import About from './components/About';
+import Textform from './components/Textform';
+/* import About from './components/About'; */
 function App() {
   return (
    <>
 <Navbar title ="Textutils" About="Textutils About" Home="Home"/>  {/* props */}
 <div className="container my-3">
-  {/* <Textform heading ="Enter the text to analyze below"/>  */}
-  <About/>
+  <Textform heading ="Enter the text to analyze below"/> 
+{/*   <About/> */}
 </div>
    </>
   );
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -3,23 +3,19 @@ import React, {useState} from 'react'
 
 export default function Textform(props) {
         const handleUpClick = ()=>{
-            /* console.log("Uppercase was clicked :" +  text); */
-            let newText  = text.toUpperCase();
+
+            let newText  = text.toUpperCase();     /* for a uppercase */
             setText(newText)
         }
         const handleLoClick = ()=>{
-            /* console.log("Lowerrcase was clicked :" +  text); */
-            let newText  = text.toLowerCase();
+            let newText  = text.toLowerCase();      /* for a lowercase */
             setText(newText)
         }
-        const handleClearClick= ()=>{
-            /* console.log("Lowerrcase was clicked :" +  text); */
+        const handleClearClick= ()=>{         /* for a cleaaText */
             let newText  = '';
             setText(newText)
         }
-
         const handleOnChange = (event)=>{
-           /*  console.log("On change"); */
             setText(event.target.value)
         }
 
@@ -31,9 +27,9 @@ export default function Textform(props) {
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>       {/*  */}
-        <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>       {/*  */}
+        <button className="btn btn-success mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-success mx-2" onClick={handleLoClick}>Convert to Lowercase</button>       {/*  */}
+        <button className="btn btn-success mx-2" onClick={handleClearClick}>Clear Text</button>       {/*  */}
   </div>
 
   <div className="container my-3">
