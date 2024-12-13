b6f7d79482c90f3bb6bda4bfd0e046e4f5e56d41
Milind Sangale <smilind900@gmail.com>
2024-12-13 23:29:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
-/* import About from './components/About'; */
+import React, {useState} from 'react';
+import About from './components/About';
 function App() {
+  const [mode, setMode] = useState('dark');         /* usestate */
+   
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+     /*  document.body.style.backgroundColor = '#042743'; */
+    /*   showAlert("Dark mode has been enabled", "success"); */
+  }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor = 'white';
+    /* showAlert("Light mode has been enabled", "success"); */
+  }
+}
   return (
    <>
-<Navbar title ="Textutils" About="Textutils About" Home="Home"/>  {/* props */}
+<Navbar title ="Textutils" mode={mode} toggleMode={toggleMode}
+About="Textutils About" Home="Home"/>  {/* props */}
 <div className="container my-3">
   <Textform heading ="Enter the text to analyze below"/> 
-{/*   <About/> */}
+ {/*  <About/> */}
 </div>
    </>
   );
 }
 export default App;
+
+
