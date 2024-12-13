876cf848da17e5930c6edb4024be5a0705bdd5bf
Milind Sangale <smilind900@gmail.com>
2024-12-13 19:54:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+/* import Textform from './components/Textform'; */
+import About from './components/About';
 function App() {
   return (
    <>
 <Navbar title ="Textutils" About="Textutils About" Home="Home"/>  {/* props */}
 <div className="container my-3">
-  <Textform heading ="Enter the text to analyze below"/> 
+  {/* <Textform heading ="Enter the text to analyze below"/>  */}
+  <About/>
 </div>
    </>
   );
